a323825589eb04aba45f1f9d06b866f436c4bfed
AlexeyMad <alexey.abdullin1@gmail.com>
2016-11-19 15:05:34 +0300

    doesnt work now, but i will end it asap

--- practice 1/task1/task1.cpp
+++ practice 1/task1/task1.cpp
@@ -12,7 +12,7 @@
 
 int main()
 {
-
+	float we, he, ratio;
 	char sex;
 
 
@@ -26,7 +26,30 @@ int main()
 		printf("Enter your sex(m/f):");
 		scanf_s("%c", &sex);
 	}
- 
+
+
+	printf("Weight(kg): ");
+
+	while ((we <= 0) || (we >= 300))
+	{
+		rewind(stdin);
+		printf("Do you think its funny? If you weigh more than 300 kg or less than 0, you should seek medical attention. \nOr u can try again:\n");
+		printf("Weight(kg): ");
+		scanf_s("%f", &we);
+	}
+	printf("Heigth(meters): ");
+	scanf_s("%f", &he);
+
+	while ((he) <= 0 || (he >= 3))
+	{
+		rewind(stdin);
+		printf("Your heigth is invalid, enter your heigth(m) again: ");
+		scanf_s("%f", &he);
+	}
+	ratio = we / (he * he);
+
+	printf("\n", ratio);
+
 	getchar();
 	getchar();
 	return 0;
