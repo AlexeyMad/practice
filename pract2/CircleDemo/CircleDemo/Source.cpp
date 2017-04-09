1d6023cc8ad3e3b47123441d7cd25b80fc815ef1
DESKTOP-RB0KMC5\pc <stoller@devcorp.ru>
2017-04-09 14:56:07 +0300

    w

--- pract2/CircleDemo/CircleDemo/Source.cpp
+++ pract2/CircleDemo/CircleDemo/Source.cpp
@@ -1,12 +1,33 @@
 #include <conio.h>
 #include <iostream>
 #include "Circle.h"
-
-
+#include <math.h>
+#include <cmath>
 using std::cout;
 using std::endl;
 int main ()
-{
+{	/*����������, ��� ����� ����� ����� ���������� ����.
+	������ ����������� ������� ���� ���� �������� ������
+	(����� ��� � ������������ ��� �������� ������).��� - �� 
+	��������� ������ � ������������ ����� � ��������� ����� 
+	������ ������ 1 ����.����� ������� ����� ������������ �����
+	� ������� ��������� �����, ��������� ����������� �����.�����
+	�������� ����� ������.������ �� ������ ����� ���������� � 6378.1 ��*/
+	setlocale(0, "");
+	Circle Earth, Rope;
+	Earth.setRadius(6378.1);
+	cout << "******Earth********" << endl;
+	cout << "radius\t"<< (int)Earth.Radius <<"(km)"<< endl;
+	cout << "Ference\t"<< (int)Earth.Ference << endl;
+	cout << "Area\t" <<  (int)Earth.Area << endl;
+
+	Rope.setFerence(0.001);
+	cout << "******Rope********" << endl;
+	cout << "radius\t" << Rope.Radius << endl;
+	cout <<"Ference\t" << Rope.Ference << endl;
+	cout << "Area\t"   << Rope.Area << endl;
+	Rope.setArea((int)Earth.get_Ference() + Rope.get_Ference());
+	cout << "�������:\t" << Rope.get_Radius() - (int)Earth.get_Radius() << endl;
 	_getch();
 	return 0;
 
--- pract2/CircleDemo/CircleDemo/circle.h
+++ pract2/CircleDemo/CircleDemo/circle.h
@@ -1,9 +1,50 @@
 #include <iostream>
+#include <math.h>
+#include <conio.h>
 #include <cmath>
 #define pi 3.14159
 
 class Circle {
-private:
 
+
+	/*
+	Radius - ������ 
+	Ference - ����� ���������� 
+	Area - ������� ����� */
+
+public:
+
+	void setRadius(double set_Rad) {
+
+		Radius = set_Rad;
+		Ference = 2 * pi * Radius;
+		Area = pi *  pow(Radius, 2);
+	}
+
+	void setFerence(double set_Fer) {
+
+		Ference = set_Fer;
+		Radius = Ference / (2 * pi);
+		Area = pi * pow(Radius, 2);
+	}
+
+	void setArea(double set_Ar) {
+
+		Area = set_Ar;
+		Radius = sqrt(Area / pi);
+		Ference = 2 * pi * Radius;
+	}
+
+	double get_Radius() {
+		return Radius;
+	}
+	double get_Ference() {
+		return Ference;
+	}
+	double get_Area() {
+		return Area;
+	}
+	public:
 	double Radius, Ference, Area;
+
 };
